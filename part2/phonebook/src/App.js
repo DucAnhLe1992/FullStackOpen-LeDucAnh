75564281ca837862c02bfe47ee73005758bc8830
Le Duc Anh <alexdal1992@gmail.com>
2021-04-11 20:38:24 +0300

    part2-completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import "./index.css";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
 import Persons from "./components/Persons";
 import personServices from "./services/personServices";
-import axios from "axios";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+  const [notif, setNotif] = useState({ message: null, type: null });
 
   useEffect(() => {
     personServices.getAllPersons().then((data) => setPersons(data));
@@ -26,6 +28,7 @@ const App = () => {
         setPersons(persons.concat(newPerson));
         setNewName("");
         setNewNumber("");
+        setNotification(`Added ${newName}`, "successStyle");
       });
     } else {
       const needReplace = window.confirm(
@@ -36,7 +39,22 @@ const App = () => {
         personServices
           .changePersonNumber(doesExists.id, changedPerson)
           .then((res) => {
-            setPersons(persons.map(person => person.id !== doesExists.id ? person : res.data));
+            setPersons(
+              persons.map((person) =>
+                person.id !== doesExists.id ? person : res.data
+              )
+            );
+            setNotification(
+              `Number of ${changedPerson.name} changed successfully`,
+              "successStyle"
+            );
+          })
+          .catch((error) => {
+            setNotification(
+              `Person ${changedPerson.name} not found!`,
+              "errorStyle"
+            );
+            setPersons(persons.filter((person) => person.id !== doesExists.id));
           });
       }
     }
@@ -54,9 +72,18 @@ const App = () => {
     setFilter(event.target.value);
   };
 
+  const setNotification = (message, type) => {
+    setNotif({ message: message, type: type });
+    setTimeout(() => {
+      setNotif({ message: null, type: null });
+    }, 5000);
+  };
+
   return (
     <div>
       <h1>Phonebook</h1>
+      <Notification message={notif.message} type={notif.type} />
+
       <Filter handleFilterChange={handleFilterChange} filter={filter} />
 
       <PersonForm
